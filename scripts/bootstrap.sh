e457c6c81aed90d7bf69e51c001083c644a8c1db
J Evelyn Quintana <q@world.oberlin.edu>
2018-10-23 22:01:15 -0700

    adds a bootstrapping script

---
+++ scripts/bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "$BBL_GCP_SERVICE_ACCOUNT_KEY" ]; then
+  if [ -z "$1" ]; then
+    echo "please set BBL_GCP_SERVICE_ACCOUNT_KEY as the path to your key, or pass it in as an argument to this script"
+    exit 1
+  else
+    export BBL_GCP_SERVICE_ACCOUNT_KEY="$1"
+  fi
+fi
+
+bbl plan
