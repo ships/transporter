13b16708a86df97f7eb318c14e34e14bf54e2b0b
J Evelyn Quintana <q@world.oberlin.edu>
2018-10-29 13:08:20 -0700

    slightly improved secret management

--- installations/openvpn-server/deploy.sh
+++ installations/openvpn-server/deploy.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -ex
 
+this_dir="${REPO_ROOT}/installations/openvpn-server"
+
+if [ ! -f "${this_dir}/variables.yml" ]; then
+	cat > "${this_dir}/variables.yml" <<VARS
+system_domain: ${system_domain}
+vpn_ip_from_bbl: $(dig +short "vpn.${system_domain}")
+virtual_network:
+  server_ip: 10.168.0.1
+  start_ip: 10.168.0.0
+  netmask: 255.255.255.0
+dh_pem: "$(openssl dhparam -text -outform PEM 2>/dev/null | awk '/BEGIN/{p=1}p')"
+VARS
+fi
+
 bosh deploy $REPO_ROOT/installations/openvpn-server/manifest.yml \
   -d vpn \
   --vars-file $REPO_ROOT/installations/openvpn-server/variables.yml
